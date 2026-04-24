8631cbf7a4d865075f3aa08b4cd04f429cbfbe0d
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-04-25 00:02:46 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,13 @@ repositories {
     mavenCentral()
 }
 
-private val vertx = "5.0.10"
-
 dependencies {
+    val vertx = "5.0.11"
+    val jUnit = "6.0.3"
     implementation("io.vertx:vertx-core:$vertx")
     testImplementation("io.vertx:vertx-unit:$vertx")
 
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation(platform("org.junit:junit-bom:$jUnit"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
