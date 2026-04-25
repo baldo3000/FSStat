c325b80be6806a68f9510dd6d92c65a093df9a35
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-04-25 11:31:19 +0200

    Improve Async version main

--- src/main/java/me/baldo3000/async/Async.java
+++ src/main/java/me/baldo3000/async/Async.java
@@ -1,5 +1,6 @@
 package me.baldo3000.async;
 
+import io.vertx.core.Vertx;
 import me.baldo3000.async.impl.AsyncFSStatImpl;
 
 import java.nio.file.Paths;
@@ -7,13 +8,19 @@ import java.nio.file.Paths;
 public class Async {
     static void main() {
         IO.println("Hello World!");
-
-        var fSStat = new AsyncFSStatImpl();
-        var testPath = Paths.get("C:/Users/andre/Documents/PROGRAMMAZIONE/PCD");
-        fSStat.getFSReport(testPath).onSuccess(files -> {
-                    IO.println("Files found: " + files.size());
-                    fSStat.close();
-                }
-        ).onFailure(System.err::print);
+        var vertx = Vertx.vertx();
+        var fSStat = new AsyncFSStatImpl(vertx);
+        var testPath = Paths.get("C:/Users/andre/Documents/PROGRAMMAZIONE");
+        var start = System.currentTimeMillis();
+        fSStat.getFSReport(testPath).onSuccess(f -> {
+            IO.println("Files found: " + f);
+            System.out.println("Total time taken: " + (System.currentTimeMillis() - start) / 1000.0 + "s");
+        }).onFailure(System.err::print).onComplete(_ -> vertx.close());
+//        Future.all(
+//                fSStat.getFSReport(testPath).onSuccess(f -> IO.println("Files found: " + f)),
+//                fSStat.getFSReport(testPath).onSuccess(f -> IO.println("Files found: " + f))
+//        ).onSuccess(_ ->
+//                System.out.println("Total time taken: " + (System.currentTimeMillis() - start) / 1000.0 + "s")
+//        ).onFailure(System.err::print).onComplete(_ -> vertx.close());
     }
 }
