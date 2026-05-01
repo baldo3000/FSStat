66d772bfc98b9764be74de2d988ae0a794d451b9
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-05-01 11:05:50 +0200

    Prepare for RxJava implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,11 @@ repositories {
 dependencies {
     val vertx = "5.0.11"
     val jUnit = "6.0.3"
+    val rxJava = "4.0.0-alpha-13"
     implementation("io.vertx:vertx-core:$vertx")
-    testImplementation("io.vertx:vertx-unit:$vertx")
+    implementation("io.reactivex.rxjava4:rxjava:$rxJava")
 
+    testImplementation("io.vertx:vertx-unit:$vertx")
     testImplementation(platform("org.junit:junit-bom:$jUnit"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ src/main/java/me/baldo3000/rx/Rx.java
@@ -0,0 +1,7 @@
+package me.baldo3000.rx;
+
+public class Rx {
+    static void main() {
+        IO.println("Hello World!");
+    }
+}
