79c28632c058a20619e8d9452b3bb09a0e9c73ac
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-05-14 20:15:34 +0200

    rewrite RxJava implementation to allow cancellation

--- src/main/java/me/baldo3000/rx/impl/RxFSStatImpl.java
+++ src/main/java/me/baldo3000/rx/impl/RxFSStatImpl.java
@@ -1,7 +1,7 @@
 package me.baldo3000.rx.impl;
 
 import io.reactivex.rxjava4.core.Flowable;
-import io.reactivex.rxjava4.disposables.Disposable;
+import io.reactivex.rxjava4.schedulers.Schedulers;
 import me.baldo3000.common.api.FSReport;
 import me.baldo3000.common.impl.ArrayFSReport;
 import me.baldo3000.rx.api.RxFSStat;
@@ -13,51 +13,54 @@ import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.List;
 
 public class RxFSStatImpl implements RxFSStat {
 
     @Override
     public Flowable<FSReport> getFSReport(Path directory, long maxFileSize, int bands) {
-        return Flowable.virtualCreate(emitter -> {
-            var report = new ArrayFSReport(directory, maxFileSize, bands);
-
-            var cancelled = new AtomicBoolean(false);
-            emitter.canceller().add(Disposable.fromRunnable(() -> cancelled.set(true)));
-
+        return Flowable.defer(() -> {
             try {
-                var attrs = Files.readAttributes(directory, BasicFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
+                // Read directory attributes
+                final var attrs = Files.readAttributes(
+                        directory, BasicFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
 
+                // If it's a file, create a report with just that file
                 if (attrs.isRegularFile()) {
-                    emitter.emit(report.countFileBySize(attrs.size()));
-                } else if (attrs.isDirectory()) {
-                    try (var dirStream = Files.list(directory)) {
-                        var subPaths = dirStream.toList();
-                        var childReports = new HashMap<Path, FSReport>(subPaths.size() * 2);
-                        Flowable.fromIterable(subPaths)
-                                .flatMap(subPath -> getFSReport(subPath, maxFileSize, bands))
-                                .blockingSubscribe(childReport -> {
-                                            FSReport previous = childReports.put(childReport.getDirectory(), childReport);
-                                            if (previous != null) {
-                                                report.subtract(previous);
-                                            }
-                                            report.merge(childReport);
-                                            if (!cancelled.get()) {
-                                                emitter.emit(report.copy());
-                                            }
-                                        },
-                                        error -> {
-                                            if (!cancelled.get()) {
-                                                log("Traversal error in " + directory + ": " + error.getMessage());
-                                            }
-                                        });
-                    }
+                    var report = new ArrayFSReport(directory, maxFileSize, bands);
+                    return Flowable.just(report.countFileBySize(attrs.size()));
                 }
+
+                // If it's a directories, create a report dynamically merging the subDirectories reports
+                if (attrs.isDirectory()) {
+                    final List<Path> subPaths;
+                    try (var dirStream = Files.list(directory)) {
+                        subPaths = dirStream.toList();
+                    }
+
+                    var parentReport = new ArrayFSReport(directory, maxFileSize, bands);
+                    var childReports = new HashMap<Path, FSReport>(subPaths.size() * 2);
+
+                    // FlatMap guarantees serial delivery to all downstream operators including map
+                    // Access to parentReport and childReports is done sequentially
+                    return Flowable.fromIterable(subPaths)
+                            .flatMap(subPath -> getFSReport(subPath, maxFileSize, bands))
+                            .map(childReport -> {
+                                final FSReport previous = childReports.put(
+                                        childReport.getDirectory(), childReport);
+                                if (previous != null) parentReport.subtract(previous);
+                                parentReport.merge(childReport);
+                                return parentReport.copy();
+                            });
+                }
+
             } catch (IOException | UncheckedIOException e) {
                 log("Skipping " + directory + ": " + e.getMessage());
-                emitter.emit(report); // Emit empty report
             }
-        });
+
+            // Default case if error is thrown or the directory is a symlink
+            return Flowable.empty();
+        }).subscribeOn(Schedulers.virtual()); // Subscribe on virtual threads schedulers for blocking operations
     }
 
     private void log(String msg) {
